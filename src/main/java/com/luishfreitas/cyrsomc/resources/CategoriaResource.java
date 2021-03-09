5e00a1166b6e407cc5ec450ed28b3643b1b629f3
Luis Freitas <luish.silveira@hotmail.com>
2021-03-08 22:10:33 -0300

    Testando classe dominio Categoria

--- src/main/java/com/luishfreitas/cyrsomc/resources/CategoriaResource.java
+++ src/main/java/com/luishfreitas/cyrsomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.luishfreitas.cyrsomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luishfreitas.cyrsomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST funcionando!!!";
+	public List<Categoria> listar() {
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		Categoria cat1 = new Categoria(1, "Infomática");
+		Categoria cat2 = new Categoria(2, "Cama e Banho");
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
+		
 	}
 
 }
