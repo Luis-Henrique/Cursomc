00196f967e9c5457831a34a604cb0f6d1f6e5a1b
Luis Freitas <luish.silveira@hotmail.com>
2021-04-25 19:44:06 -0300

    Adicionando CategoriaDTO

--- src/main/java/com/luishfreitas/cyrsomc/resources/CategoriaResource.java
+++ src/main/java/com/luishfreitas/cyrsomc/resources/CategoriaResource.java
@@ -3,7 +3,9 @@ package com.luishfreitas.cyrsomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luishfreitas.cyrsomc.domain.Categoria;
+import com.luishfreitas.cyrsomc.dto.CategoriaDTO;
 import com.luishfreitas.cyrsomc.services.CategoriaService;
 
 @RestController
@@ -58,6 +61,17 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	
+	}
+	
+	//expoe com verbo http
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+		
 	}
 
 }
--- src/main/java/com/luishfreitas/cyrsomc/services/CategoriaService.java
+++ src/main/java/com/luishfreitas/cyrsomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.luishfreitas.cyrsomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,12 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DateIntegrityException("nao é possível remover uma categoria com produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+
+		return repo.findAll();
+
 	}
 
 }
