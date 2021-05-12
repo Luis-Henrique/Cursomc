f5e4114316d1bb543903f73416f82f7c5bbc929a
Luis Freitas <luish.silveira@hotmail.com>
2021-05-12 20:47:10 -0300

    Validacao CPF e CNPJ

---
+++ src/main/java/com/luishfreitas/cyrsomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,42 @@
+package com.luishfreitas.cyrsomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.luishfreitas.cyrsomc.domain.enums.TipoCliente;
+import com.luishfreitas.cyrsomc.dto.ClienteNewDTO;
+import com.luishfreitas.cyrsomc.resources.exceptions.FieldMessage;
+import com.luishfreitas.cyrsomc.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+	
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CPF invalido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CNPJ invalido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+			.addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+		
+	}
+
+}
